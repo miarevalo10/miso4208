dfa84a2407ce0024d4b5bc4ff6617a95e22d9aca
Maria Arevalo <mi.arevalo10@uniandes.edu.co>
2019-03-24 22:27:40 -0500

    fix overwrite image

--- backend/vrtWorker.js
+++ backend/vrtWorker.js
@@ -7,7 +7,7 @@ const multerS3 = require('multer-s3');
 
 var params = {
     QueueUrl: process.env.SQS_VRT
-  };
+};
 
 AWS.config.update({ region: 'us-west-2' });
 
@@ -16,30 +16,30 @@ var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
 
 var fileName1 = "./images/v1";
-var fileName2= "./images/v2";
+var fileName2 = "./images/v2";
 
 var t = setInterval(rcvMsg, 2000);
 
 function rcvMsg() {
     sqs.receiveMessage(params, function (err, data) {
-      if (err) console.log(err, err.stack);
-      else {
-        if (data.Messages) {
-          var test = data.Messages[0].Body;
-          console.log('msg rcv', JSON.parse(test));
-          receiptHandle = data.Messages[0].ReceiptHandle;
-          filenames = [JSON.parse(test).fileName1,JSON.parse(test).fileName2]
-          downloadImages(filenames);
-          
-  
-        } else {
-          console.log('no new msgs');
-        }
-      }
-    });
-  }
+        if (err) console.log(err, err.stack);
+        else {
+            if (data.Messages) {
+                var test = data.Messages[0].Body;
+                console.log('msg rcv', JSON.parse(test));
+                receiptHandle = data.Messages[0].ReceiptHandle;
+                filenames = [JSON.parse(test).fileName1, JSON.parse(test).fileName2]
+                downloadImages(filenames);
 
-async function getDiff(img1,img2) {
+
+            } else {
+                console.log('no new msgs');
+            }
+        }
+    });
+}
+
+async function getDiff(img1, img2) {
     const options = {
         output: {
             errorColor: {
@@ -66,54 +66,58 @@ async function getDiff(img1,img2) {
     );
 
     await fs.writeFile("./images/output.png", data.getBuffer());
+    uploadImage("./images/output.png");
 }
 
 //Subir screenshots a s3
-const upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: 'pruebas-autom/vrt',
-      acl: 'public-read',
-      metadata: function (req, file, cb) {
-        cb(null, {metadata: 'test'});
-      },
-      key: function (req, file, cb) {
-        console.log('file',file);
-        cb(null, Date.now().toString());
-      }
-    })
-  })
+function uploadImage(file) {
+    fs.readFile(file, function (err, data) {
+        if (err) { throw err; }
+        var params = {
+            Bucket: 'pruebas-autom',
+            Key: `vrt/test/output.png`,
+            Body: data
+        };
 
+        s3.putObject(params, function (err, data) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("Successfully uploaded data");
+            }
 
+        });
 
-// Downloads features zip and saves it to calabash folder
-async function downloadImages(fileNames) {
-    var fileName = fileNames.pop();
+    });
+}
 
-    var params = {
-      Bucket: 'pruebas-autom',
-      Key: `vrt/test/${fileName}`
-    };
-    console.log('keyyyy', params.Key);
-    shell.mkdir('images');
-    const filePath = `./images/${fileName}`;
-    s3.getObject(params, (err, data) => {
-      if (err) console.error(err)
-      else {
-        console.log('Starting images download');
-        fs.writeFile(filePath, data.Body, (err) => {
-          shell.exec(`unzip images/${fileName} -d images`);
-          console.log(`${filePath} has been created!`)
-          if(fileNames.length==0){
-            console.log('length 0')
+    // Downloads features zip and saves it to calabash folder
+    async function downloadImages(fileNames) {
+        var fileName = fileNames.pop();
 
-            getDiff(`${fileName1}/test1.jpg`,`${fileName2}/test2.jpg`);
-        } else {
-            console.log('length >0')
+        var params = {
+            Bucket: 'pruebas-autom',
+            Key: `vrt/test/${fileName}`
+        };
+        console.log('keyyyy', params.Key);
+        shell.mkdir('images');
+        const filePath = `./images/${fileName}`;
+        s3.getObject(params, (err, data) => {
+            if (err) console.error(err)
+            else {
+                console.log('Starting images download');
+                fs.writeFile(filePath, data.Body, (err) => {
+                    shell.exec(`unzip -o images/${fileName} -d images`);
+                    console.log(`${filePath} has been created!`)
+                    if (fileNames.length == 0) {
+                        console.log('length 0')
+                        getDiff(`${fileName1}/test1.jpg`, `${fileName2}/test2.jpg`);
+                    } else {
+                        console.log('length >0')
+                        downloadImages(filenames);
+                    }
+                })
+            }
+        })
+    }
 
-            downloadImages(filenames);
-        }
-        }) 
-      }
-    })
-  }
