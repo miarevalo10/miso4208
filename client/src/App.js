7df033d78f5565f5d3ba9461137d4355a31fff52
Maria Arevalo <mi.arevalo10@uniandes.edu.co>
2019-04-27 10:19:48 -0500

    Navbar fixes

--- client/src/App.js
+++ client/src/App.js
@@ -14,36 +14,34 @@ class App extends Component {
     return (
       <Router>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">Navbar</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul lass="navbar-nav mr-auto">
-            <li className="nav-item active">
-                <Link  to="/new-test">New test</Link>
+            <ul className="nav navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link to="/vrt">vrt</Link>
               </li>
               <li className="nav-item active">
-                <Link  to="/">vrt</Link>
-              </li>
-              <li className="nav-item active">
-                <Link  to="/about">Reportes</Link>
+                <Link to="/about">Reportes</Link>
               </li >
               <li className="nav-item active">
-                <Link  to="/sendmessage">Home</Link>
+                <Link to="/home">Home</Link>
               </li >
-              <li className="nav-item active">
-                <Link  to="/create-application">Create application</Link>
+              <li>
+              <li><a href="/create-application">Create application</a></li>
+
+                {/* <Link to="/create-application">Create application</Link> */}
               </li>
             </ul>
           </div>
         </nav>
         <br />
-        
-        <Route exact path="/" component={Vrtscreen} />
+
+        <Route exact path="/vrt" component={Vrtscreen} />
         <Route path="/about" component={Reportscreen} />
         <Route path="/details" component={ApplicationDetail} />
-        <Route path="/sendmessage" component={Home} />
+        <Route path="/home" component={Home} />
         <Route path="/topics" component={Topics} />
         <Route path="/new-test" component={NewTestRun} />
         <Route path="/create-application" component={CreateApplication} />
