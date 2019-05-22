10bf4b0de62c6ca3e9ccf629951a6ca874632199
Maria Arevalo <mi.arevalo10@uniandes.edu.co>
2019-05-21 23:11:12 -0500

    vrt worker subiendo imagenes a s3

--- backend/vrtWorker.js
+++ backend/vrtWorker.js
@@ -53,8 +53,6 @@ imgKeys = [];
 
 //var t = setInterval(rcvMsg, 2000);
 
-
-
 function rcvMsg() {
     sqs.receiveMessage(params, function (err, data) {
         if (err) console.log(err, err.stack);
@@ -65,8 +63,7 @@ function rcvMsg() {
                 receiptHandle = data.Messages[0].ReceiptHandle;
                 testObj = JSON.parse(test);
                 runVrt(testObj);
-                //                downloadAll({ apkv1: 'me.kuehle.carreport_79.apk', apkv2: 'me.kuehle.carreport_79.apk', features: 'test/features.zip' })
-                //downloadApk(JSON.parse(test));
+                deleteMessage();
             } else {
                 console.log('no new msgs');
             }
@@ -104,8 +101,9 @@ function listFeatures(data) {
                     imgKeys.push(fileArr[1]);
                 }
             });
-            console.log('KEYS', imgKeys);
             buildJson(testData);
+            compareAllImages();
+            
         }
     });
 }
@@ -115,81 +113,102 @@ function getS3Path(projectId, versionId, processId) {
 }
 
 function buildJson(data) {
-    console.log('que llega al json', data)
     var v1path = getS3Path(data.projectId, data.versionOneId, data.processOneId);
     var v2path = getS3Path(data.projectId, data.versionTwoId, data.processTwoId);
-    var diffPath = `${FOLDER_S3}${data.projectId}/vrt/${data.vrtProcessId}/screenshots`
+    var diffPath = `${FOLDER_S3}${data.projectId}/vrt/${data.vrtProcessId}/screenshots`;
 
-
-    imgKeys.forEach( img => {
+    imgKeys.forEach(img => {
         var imgArr = img.split('/');
         var featName = imgArr[0];
         var scenarioName = imgArr[1];
-        var imgName = imgArr [2];
+        var imgName = imgArr[2];
         var v1ImgPath = `${v1path}/${img}`;
         var v2ImgPath = `${v2path}/${img}`;
         var diffImgPath = `${diffPath}/${img}`
-        
-        if(!(_.find(report, {name:featName}))){
-            report.push({name: featName, scenarios: []});
-        }
-        
-        var scenarios = _.find(report, {name:featName}).scenarios;
-        if(!(_.find(scenarios, {name:scenarioName}))){
-            scenarios.push({name: scenarioName, screenshots: []});
+
+        if (!(_.find(report, { name: featName }))) {
+            report.push({ name: featName, scenarios: [] });
         }
 
-        var screenshots = _.find(scenarios, {name:scenarioName}).screenshots;
-        screenshots.push({name: imgName, v1: v1ImgPath, v2:v2ImgPath, diff: diffImgPath});
-        console.log('screenshots', screenshots);
+        var scenarios = _.find(report, { name: featName }).scenarios;
+        if (!(_.find(scenarios, { name: scenarioName }))) {
+            scenarios.push({ name: scenarioName, screenshots: [] });
+        }
+
+        var screenshots = _.find(scenarios, { name: scenarioName }).screenshots;
+        screenshots.push({ name: imgName, v1: v1ImgPath, v2: v2ImgPath, diff: diffImgPath });
     });
 
-    fs.writeFile('report.json', JSON.stringify(report), 'utf8', (err) => {
-        console.log('error', err);
+    fs.writeFileSync('report.json', JSON.stringify(report), 'utf8');
+    uploadFile('report.json', `cypress/${data.projectId}/vrt/${data.vrtProcessId}/report.json`, 'application/json');
+}
+
+
+async function compareAllImages(test) {
+
+    report.forEach(feature => {
+
+        if (!fs.existsSync(feature.name)) {
+            fs.mkdirSync(feature.name);
+        }
+        shell.cd(feature.name);
+
+        feature.scenarios.forEach(scenario => {
+            if (!fs.existsSync(scenario.name)) {
+                fs.mkdirSync(scenario.name);
+            }
+            shell.cd(scenario.name);
+
+            scenario.screenshots.forEach(screenshot => {
+                var name = screenshot.name.split('.png')[0];
+                if (!fs.existsSync(name)) {
+                    fs.mkdirSync(name);
+                }
+                shell.cd(name);
+                var img1Path = `${feature.name}/${scenario.name}/${name}/v1-${screenshot.name}`;
+                var img2Path = `${feature.name}/${scenario.name}/${name}/v2-${screenshot.name}`;
+                var diffPath = `${feature.name}/${scenario.name}/${name}/diff-${screenshot.name}`;
+
+                async function downloadImages() {
+                    await downloadImage(screenshot.v1, `${feature.name}/${scenario.name}/${name}/v1-${screenshot.name}`);
+                    await downloadImage(screenshot.v2, `${feature.name}/${scenario.name}/${name}/v2-${screenshot.name}`);
+                    getDiff(img1Path, img2Path, diffPath, screenshot.diff);
+                }               
+
+                downloadImages();
+
+                shell.cd('..');
+
+            });
+            shell.cd('..');
+        });
+        shell.cd('..');
+
+    });
+}
+
+function downloadImage(key, imgName) {
+
+    var params = {
+        Bucket: 'pruebas-autom',
+        Key: key
+    };
+
+
+    return new Promise((resolve, reject) => s3.getObject(params, (err, data) => {
+        if (err)  {
+            reject(err);
+        } 
+        else {
+            fs.writeFileSync(imgName, data.Body);
+            resolve(data);
+        }
     })
+    );
 }
 
+async function getDiff(img1, img2, output, s3key) {
 
-
-
-
-
-
-
-
-
-
-
-
-function compareAllImages(test) {
-    if (!fs.existsSync('output')) {
-        fs.mkdirSync('output');
-    }
-    fs.readdirSync('v1').forEach(file => {
-        var ofileName = file.substr(0, file.lastIndexOf(".")) + ".png";
-        getDiff(`./v1/${file}`, `./v2/${file}`, ofileName);
-    });
-    saveDB(test);
-    deleteMessage();
-}
-
-function saveDB(test) {
-    let test = new VRTModel({
-        timestamp: Date.now(),
-        apkV1: test.apkv1,
-        apkV2: test.apkv2,
-        output: 'vrt/test/output'
-    });
-
-    test.save()
-        .then(doc => {
-            console.log('doc saved', doc);
-        })
-        .catch(err => {
-            console.error(err)
-        })
-}
-async function getDiff(img1, img2, output) {
     const options = {
         output: {
             errorColor: {
@@ -212,25 +231,27 @@ async function getDiff(img1, img2, output) {
         fs.readFileSync(img2),
         options
     );
-    fs.writeFileSync("./output/" + output, data.getBuffer());
+    fs.writeFileSync(output, data.getBuffer());
 
-    uploadImage("./output/" + output, output);
+    uploadFile(output, s3key, 'image/png');
 }
 
-function uploadImage(file, name) {
+function uploadFile(file, s3key, contentType) {
     fs.readFile(file, function (err, data) {
         if (err) { throw err; }
         var params = {
             Bucket: 'pruebas-autom',
-            Key: `vrt/test/output/${name}`,
-            Body: data
+            Key: s3key,
+            Body: data,
+            ContentType: contentType,
+            ACL: 'public-read'
         };
 
         s3.putObject(params, function (err, data) {
             if (err) {
                 console.log(err)
             } else {
-                console.log("Successfully uploaded data");
+                console.log("Successfully uploaded data ", s3key);
             }
 
         });
@@ -254,26 +275,5 @@ function deleteMessage() {
 }
 
 
-function downloadImages(test) {
 
-    var params = {
-        Bucket: 'pruebas-autom',
-        Key: `vrt/test/${test.imgLocation}`
-    };
-    console.log('key', params.Key);
-    if (!fs.existsSync(basePath)) {
-        fs.mkdirSync(basePath);
-    }
-    const filePath = basePath + test.imgLocation
-    s3.getObject(params, (err, data) => {
-        if (err) console.error(err)
-        else {
-
-            console.log('Starting images download');
-            fs.writeFileSync(filePath, data.Body);
-            unzipFile(test.imgLocation);
-            compareAllImages('images');
-        }
-    })
-}
 
